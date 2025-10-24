69de63c918ae444623f23ec231b19fba9b9110ed
Akeeb Wuraola Ayomide <86525393+AkeebWuraola@users.noreply.github.com>
2025-10-24 01:47:39 +0100

    Add bulk insert procedures for taxi trip data

---
+++ SQL Scripts/Bronze/proc_load_bronze.sql
@@ -0,0 +1,17 @@
+bulk insert bronze.yellow_taxi_trip
+from 'C:\Users\wuraola.akeeb\OneDrive - First City Monument Bank (FCMB)\Documents\PORTFOLIO\yellow_tripdata\csv_trip_data\yellow_tripdata_2024-01.csv'
+with (
+	firstrow = 2, --specifies the beginning of your data
+	fieldterminator = ',', --specifies the delimeter
+	rowterminator = '0x0a',
+	tablock --locks the table while insert is ongoing
+);
+
+bulk insert bronze.green_taxi_trip
+from 'C:\Users\wuraola.akeeb\OneDrive - First City Monument Bank (FCMB)\Documents\PORTFOLIO\green_tripdata\csv_trip_data\green_tripdata_2024-01.csv'
+with (
+	firstrow = 2, --specifies the beginning of your data
+	fieldterminator = ',', --specifies the delimeter
+	rowterminator = '0x0a',
+	tablock --locks the table while insert is ongoing
+);
