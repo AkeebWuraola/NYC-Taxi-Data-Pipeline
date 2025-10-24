082d67efb6bc0f97bc0df88ac8579dd7103f4f4f
Akeeb Wuraola Ayomide <86525393+AkeebWuraola@users.noreply.github.com>
2025-10-25 00:13:31 +0100

    Refactor bronze data loading procedure
    Refactored the stored procedure to load data into the bronze layer from CSV files. Added error handling and logging for the loading process.

--- SQL Scripts/Bronze/proc_load_bronze.sql
+++ SQL Scripts/Bronze/proc_load_bronze.sql
@@ -8,42 +8,85 @@ It Uses the 'bulk insert' to load data from csv files to the tables
 Useful comment has been included
 =====================================================================================
 */
-declare @path nvarchar(max) 
-declare @file nvarchar(255)
-declare @cmd nvarchar(max)
+create or alter procedure bronze.stp_load_bronze as 
 
-set @path = N'C:\Users\wuraola.akeeb\OneDrive - First City Monument Bank (FCMB)\Documents\PORTFOLIO\yellow_tripdata\csv_trip_data\'
+begin 
+    
+    begin try 
+        declare @start_time datetime,@end_time datetime;
+        declare @prd_start_time datetime, @prd_end_time datetime;
+        declare @path nvarchar(max) 
+        declare @file nvarchar(255)
+        declare @cmd nvarchar(max)
+        declare @row_count int
 
-drop table if exists #files
-create table #files( filename nvarchar(255))
-insert into #files
-exec xp_cmdshell 'dir /b "C:\Users\wuraola.akeeb\OneDrive - First City Monument Bank (FCMB)\Documents\PORTFOLIO\yellow_tripdata\csv_trip_data\yellow_tripdata_2024-*.csv"'
+        set @prd_start_time = getdate();
+        print '======================================================================';
+	    print 'Loading Bronze Layer';
+	    print '======================================================================';
 
---create a cursor
-declare cur cursor for select filename from #files where filename is not null
-open cur
-fetch next from cur into @file
+	    print '----------------------------------------------------------------------';
+	    print 'Loading Yellow Trip Table';
+	    print '----------------------------------------------------------------------';
 
-while @@fetch_status = 0
-begin
-    set @cmd = '
-    bulk insert bronze.yellow_taxi_trip
-    from ''' + @path + @file + '''
-    with (
-        firstrow = 2,
-        fieldterminator = '','',
-        rowterminator = ''0x0a'',
-        tablock,
-        codepage = ''65001''
-    )'
-    exec(@cmd)
-    fetch next from cur into @file
+        set @start_time = getdate();
+
+        ---set the file path
+            set @path = N'C:\Users\wuraola.akeeb\OneDrive - First City Monument Bank (FCMB)\Documents\PORTFOLIO\yellow_tripdata\csv_trip_data\'
+
+            --create a temporary table to store files for each month
+            drop table if exists #files
+            create table #files( filename nvarchar(255))
+            insert into #files
+            exec xp_cmdshell 'dir /b "C:\Users\wuraola.akeeb\OneDrive - First City Monument Bank (FCMB)\Documents\PORTFOLIO\yellow_tripdata\csv_trip_data\yellow_tripdata_2024-*.csv"'
+
+            --create a cursor to iterate each file
+            declare cur cursor for select filename from #files where filename is not null
+            open cur
+            fetch next from cur into @file
+
+            --truncate existing table
+            print '>> Truncating Table: bronze.yellow_taxi_trip ';
+	        truncate table bronze.yellow_taxi_trip
+
+            --loop through all file
+            while @@fetch_status = 0
+            begin
+                set @cmd = '
+
+                print ''>> Inserting data into Table: bronze.yellow_taxi_trip ''; 
+                bulk insert bronze.yellow_taxi_trip
+                from ''' + @path + @file + '''
+                with (
+                    firstrow = 2,
+                    fieldterminator = '','',
+                    rowterminator = ''0x0a'',
+                    tablock,
+                    codepage = ''65001''
+                )'
+                exec(@cmd)
+                fetch next from cur into @file
+            end
+
+            close cur --stops the cursor from fetching more rows.
+            deallocate cur --frees up memory used by the cursor.
+            drop table #files
+        set @end_time = getdate();
+        print '>> Load Duration: ' + cast(datediff(second,@start_time,@end_time) as nvarchar) +' seconds'
+
+        set @prd_end_time = getdate();
+        print '>> ======================================================================'
+	    print '>> Load Duration of Bronze Layer: ' + cast(datediff(second,@prd_start_time,@prd_end_time) as nvarchar) +' seconds'
+	    print '>> ======================================================================'
+    end try 
+    begin catch 
+        print '======================================================================';
+        print 'Error Occured during Loading bronze layer';
+        print '======================================================================';
+        insert into log_process_status 
+        values('stored procedure','Yellow Taxi Load','bronze.yellow_taxi_trip','FAILED','',@start_time,@end_time,error_number(),error_message(),error_line(),error_procedure(),getdate())
+    end catch
 end
-
-close cur --stops the cursor from fetching more rows.
-deallocate cur --frees up memory used by the cursor.
-drop table #files
-
 /* Notes
 The command in xp_cmdshell 'dir /b' lists file names only (bare format, without size/date info). 
 A cursor is used to iterate through each filename in the temp table.
