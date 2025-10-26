358a9bbdbb2daf814b4f439a02f140682276ad9e
Akeeb Wuraola Ayomide <86525393+AkeebWuraola@users.noreply.github.com>
2025-10-26 11:52:57 +0100

    Create Notes.sql for bronze data queries
    Added SQL queries for data retrieval and table management.

---
+++ SQL Scripts/Bronze/Notes.sql
@@ -0,0 +1,15 @@
+select * from bronze.yellow_taxi_trip
+select * from [bronze].[green_taxi_trip]
+select * from bronze.etl_file_log
+select min(tpep_pickup_datetime),max(tpep_pickup_datetime) from bronze.yellow_taxi_trip
+
+select * from bronze.yellow_taxi_trip where cast(tpep_pickup_datetime as date) not between '2024-01-01' and '2024-12-31'
+--use NYCTaxiTrips
+
+truncate table bronze.yellow_taxi_trip
+truncate table bronze.etl_file_log
+--truncate table log_process_status
+
+select * from log_process_status
+--select * from bronze.etl_file_log
+select count(*) from bronze.yellow_taxi_trip
